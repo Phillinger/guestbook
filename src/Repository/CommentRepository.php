d9fdbe971500bd7d7a2f6b50db73d227b82a626b
Philip Pflästerer <philip@pflaesterer.org>
2020-07-16 08:38:06 +0200

    Finished step 18.4

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -16,53 +16,55 @@ use Doctrine\ORM\Tools\Pagination\Paginator;
  */
 class CommentRepository extends ServiceEntityRepository
 {
-    public const PAGINATOR_PER_PAGE = 2;
+	public const PAGINATOR_PER_PAGE = 2;
 
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Comment::class);
-    }
+	public function __construct(ManagerRegistry $registry)
+	{
+		parent::__construct($registry, Comment::class);
+	}
 
-    public function getCommentPaginator(Conference $conference, int $offset): Paginator
-    {
-        $query = $this->createQueryBuilder('c')
-            ->andWhere('c.conference = :conference')
-            ->setParameter('conference', $conference)
-            ->orderBy('c.createdAt', 'DESC')
-            ->setMaxResults(self::PAGINATOR_PER_PAGE)
-            ->setFirstResult($offset)
-            ->getQuery()
-        ;
+	public function getCommentPaginator(Conference $conference, int $offset): Paginator
+	{
+		$query = $this->createQueryBuilder('c')
+			->andWhere('c.conference = :conference')
+			->andWhere('c.state = :state')
+			->setParameter('conference', $conference)
+			->setParameter('state', 'published')
+			->orderBy('c.createdAt', 'DESC')
+			->setMaxResults(self::PAGINATOR_PER_PAGE)
+			->setFirstResult($offset)
+			->getQuery()
+		;
 
-        return new Paginator($query);
-    }
+		return new Paginator($query);
+	}
 
-    // /**
-    //  * @return Comment[] Returns an array of Comment objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+	// /**
+	//  * @return Comment[] Returns an array of Comment objects
+	//  */
+	/*
+	public function findByExampleField($value)
+	{
+		return $this->createQueryBuilder('c')
+			->andWhere('c.exampleField = :val')
+			->setParameter('val', $value)
+			->orderBy('c.id', 'ASC')
+			->setMaxResults(10)
+			->getQuery()
+			->getResult()
+		;
+	}
+	*/
 
-    /*
-    public function findOneBySomeField($value): ?Comment
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+	/*
+	public function findOneBySomeField($value): ?Comment
+	{
+		return $this->createQueryBuilder('c')
+			->andWhere('c.exampleField = :val')
+			->setParameter('val', $value)
+			->getQuery()
+			->getOneOrNullResult()
+		;
+	}
+	*/
 }
